45d049560023a2d28fcb6ed71752494c4fc77cba
Pranjal <pranjal2342.be21@chitkara.edu.in>
2023-10-28 13:50:48 +0530

    first

---
+++ main.js
@@ -0,0 +1,7 @@
+var typed = new Typed(".text", {
+    strings: ["Frontend Developer", "Creator", "Web Developer", "Event Organizer"],
+    typeSpeed: 100,
+    backSpeed: 100,
+    backDelay: 100,
+    loop: true
+}); 
